00ad44abf0aee0eb462a7ac6585bebd663947736
Wonmi <wonmadesign@gmail.com>
2019-02-25 15:35:49 +0900

    Initial Commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+const fs = require('fs')
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+app.use((req, res, next) => {
+    const now = new Date().toString()
+    const log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log', log + `\n`, (error) => {
+        if(error) {
+            console.log('Unable to append to server.log')
+        }
+    })
+    next()
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance')
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => { 
+    res.render('home', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!!',
+        currentYear: new Date().getFullYear()
+    })})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is ready!')
+})
